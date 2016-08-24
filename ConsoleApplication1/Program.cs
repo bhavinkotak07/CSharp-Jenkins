f17fcd932f2f8c42238a6a230a65da059012553b
unknown <Bhavin_Kotak@PUNITP396239D.ad.infosys.com>
2016-08-24 22:06:52 +0530

    Testing auto build for git commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            hrae
+            Console.WriteLine("Jenkins auto build on git commit");
             Console.ReadKey();
  
             
